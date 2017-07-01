76a8c4b22084c65ff1d271cbf06c2c2a93071b32
hakulatata <hakulatata@163.com>
2017-07-01 19:55:59 +0800

    new test about the control vesion

---
+++ camera/src/main/java/com/hakulatata/camera/adapter/A.java
@@ -0,0 +1,8 @@
+package com.hakulatata.camera.adapter;
+
+/**
+ * Created by hakulatata on 2017/7/1.
+ */
+
+public class A {
+}
---
+++ camera/src/main/java/com/hakulatata/camera/bean/B.java
@@ -0,0 +1,8 @@
+package com.hakulatata.camera.bean;
+
+/**
+ * Created by hakulatata on 2017/7/1.
+ */
+
+public class B {
+}
