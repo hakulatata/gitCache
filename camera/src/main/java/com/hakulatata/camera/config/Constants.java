ee5f62f80434e1ee1c9cc9d603b31dc96bfd3257
hakulatata <hakulatata@163.com>
2017-07-02 00:13:01 +0800

    change something without the company

--- camera/src/main/java/com/hakulatata/camera/config/Constants.java
+++ camera/src/main/java/com/hakulatata/camera/config/Constants.java
@@ -16,12 +16,7 @@ public class Constants {
         public static final int REQUEST_CODE_EDIT_PHOTO_MOSAIC = 0x105;
 
         public static final String BEOK_CAMERA_DIR = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getPath();
-        public static final String BEOK_CAMERA_TEMP_FILE = "temp.jpg";
     }
 
-    // TODO: 2017/6/25 ?  "beok/property/image1/" 地址为什么不可以更改？为什么。。。。。文件创建decodefile 失败。。你大爷
-    public static final class PackageInfo {
-        public static final String IMG_PACKAGE_NAME = "beok/property/image1/";
-        public static final String PHOTO_PACKAGE_NAME = "pmjoy/photo/";
-    }
+
 }
